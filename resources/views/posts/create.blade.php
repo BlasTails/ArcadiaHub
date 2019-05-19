70c9350349b955bf129a0163eb6608131cf2a354
BlasTails <nikko2560@gmail.com>
2019-05-19 16:09:02 +1000

    change page

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -2,58 +2,49 @@
 @section('content')
 <div class="container">
     <div calss="row">
-        <div class="col-md-6">
+        <div class="col-md-12 text-center">
             <h1>New Project</h1>
         </div>
-    </div>
+    </div><hr>
     {!! Form::open(['action' => 'PostsController@store', 'method' => 'POST', 'enctype' => 'multipart/form-data']) !!}
 
-    <div class="row" style="padding-top: 25px">
-        <div class="col-md-3 text-left" style="padding-top:10px">
+    <div class="row">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 text-left" style="padding-top:10px">
             <h4>{{Form::label('title','Project Title')}}</h4>
-        </div>
-        <div class="col-md-4 text-left">
             {{Form::text('title', '', ['class' => 'form-control', 'placeholder'=> ' Project Title'])}}
         </div>
+        <div class="col-md-2"></div>
     </div>
-    <div class="row">
-        <div class="col-md-3">
-            <label style="padding-top: 15px" for="Project_Title"><h4>Project Picture</h4></label>
-        </div>
-        <div class="form-group" style="margin-left: 8px; margin-top: 5px" >
+
+    <div class="row" >
+        <div class="col-md-2"></div>
+        <div class="col-md-8 text-left" style="padding-top:10px; margin-bottom:22px">
+            <h4>Cover Image</h4>
             {{Form::file('cover_image')}}
-            
         </div>
+        <div class="col-md-2"></div>
     </div>
+    
     <div class="row">
-        <div class="col-md-3" style="padding-top: 15px">
-            <label for="Category"><h4>Category</h4></label>
-        </div>
-        <div class="col-md-4 text-left">
-            <select style="margin-top: 15px" class="form-control" id="sel1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-            </select>
-        </div>
-    </div>
-    <div class="row" style="padding-top: 25px">
-        <div class="col-md-3">
-            <label style="padding-top: 15px" for="Project_Title"><h4>{{Form::label('description','Description Heading')}}</h4></label>
-        </div>
-        <div class="col-md-4 text-left">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 text-left" style="padding-top:10px">
+            <h4>{{Form::label('description','Description Heading')}}</h4>
             {{Form::text('description', '', ['class' => 'form-control', 'placeholder'=> ' Project Description'])}}
         </div>
+        <div class="col-md-2"></div>
     </div>
+    
     <div class="row">
-        <div class="col-md-3" style="padding-top: 15px">
-            <h4>{{Form::label('body','Description')}}</h4>
-        </div>
-        <div class="col-lg-8 text-left" style="padding-top: 15px">
+        <div class="col-md-2"></div>
+        <div class="col-md-8 text-left" style="padding-top:10px">
+            <h4>{{Form::label('body','Project Description')}}</h4>
             {{Form::textarea('body', '', ['id' => 'article-ckeditor', 'class' => 'form-control', 'placeholder'=> ' Description Body'])}}
         </div>
+        <div class="col-md-2"></div>
     </div>
+  
+    
     <div class="row">
         <div class="col-sm-6 text-center" style="padding-top: 15px">
             <a href="{{URL::asset('/posts')}}" class="btn btn-warning" role="button" >Cancel</a>       
