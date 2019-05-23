28a2d6b67788b6519cdd3e1693c035b4a97211c6
Ryomiyu <lancemiranda93@gmail.com>
2019-05-24 09:54:22 +1000

    Changes

--- resources/views/posts/StartupProfile.blade.php
+++ resources/views/posts/StartupProfile.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-12 text-center ">
             <div class="panel panel-default">
                 <div class="userprofile social ">
-                    <h2 class=mx-auto my-0 text->Ibrahim Haveanidea</h2>
+                    <h2 class=mx-auto my-0 text->{{ Auth::user()->name }} </h2>
                     <div class="userpic"> 
                         <img src="https://i.imgur.com/AGUuIAH.jpg" alt="" class="rounded-circle img-fluid" height="215" width="215"> 
                     </div>
