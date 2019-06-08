4322e869cc14bb1057f4647da7874f2650ef411c
BlasTails <nikko2560@gmail.com>
2019-06-08 17:04:05 +1000

    change startup UI

--- resources/views/posts/StartupProfile.blade.php
+++ resources/views/posts/StartupProfile.blade.php
@@ -10,7 +10,7 @@
                     
                     @foreach ($users as $user)
                     <div class="userpic"> 
-                        <img src="storage/profile_image/{{$user->profile_image}}" alt="" class="rounded-circle img-fluid" height="215" width="215"> 
+                        <img src="storage/profile_image/{{$user->profile_image}}" alt="Cinque Terre" class="rounded-circle" width="200" height="200"> 
                     </div>
                     @endforeach
 
@@ -19,39 +19,39 @@
         </div>
         <div class="col-md-4 col-sm-12 pull-right">
             <div class="panel panel-default" style="margin-bottom: 5px">
+                <div class="panel-heading">
+                <h1 class="page-header small"style="text-align:center">Start a new Idea</h1>
+                    <div id="wrapper">
+                        <a href="{{URL::asset('/posts/create')}}" class="btn btn-primary" role="button" style="padding-left:50px; padding-right:50px;"><i class="fa fa-plus-square" aria-hidden="true"></i> New Idea</a>
+                    </div>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+            <div class="panel panel-default" style="margin-bottom: 5px">
                 <div class="panel-heading" >
                     <h1 class="page-header small">Personal Details</h1>
                     @foreach ($users as $user)
                     <p class="page-subtitle small">Country: {{$user->country}}</p>
                     <p class="page-subtitle small">Location: {{$user->address}} </p>
-                    <p class="page-subtitle small">Profile Description {!!$user->user_details!!} </p>
+                    
                     @endforeach
                 </div>
             </div>
             <div class="panel panel-default" style="margin-bottom: 5px">
-                <div class="panel-heading">
-                    <div id="wrapper">
-                        <a href="{{URL::asset('/posts/create')}}" class="btn btn-primary" role="button" style="padding-left: 50px; padding-right: 50px"><i class="fa fa-plus-square" aria-hidden="true"></i> New Idea</a>
-                    </div>
-
+                <div class="panel-heading" >
+                    <h1 class="page-header small">Profile Description </h1>
+                    @foreach ($users as $user)
+                    <p class="page-subtitle small">{!!$user->user_details!!} </p>
+                    @endforeach
                 </div>
-                <div class="clearfix"></div>
             </div>
             <div class="panel panel-default">
                 <div class="panel-heading" >
-                    <h1 class="page-header small">Career Biography</h1>
-                    <p class="page-subtitle small">Highlights:</p>
-                    <div class="col-md-12">
-                        <ul class="list-group">
-                            <li class="list-group-item"><span class="fa fa-male"></span> Worked with 1000+ people</li>
-                            <li class="list-group-item"><span class="fa fa-institution"></span> 60+ offices</li>
-                            <li class="list-group-item"><span class="fa fa-user"></span> 50000+ satify customers</li>
-                            <li class="list-group-item"><span class="fa fa-clock-o"></span> Work hours many and many still counting</li>
-                            <li class="list-group-item"><span class="fa fa-heart"></span> Customer satisfaction for servics</li>
-                        </ul>
-                    </div>
+                    <h1 class="page-header small">Contact</h1>
+                    @foreach ($users as $user)
+                    <p class="page-subtitle small">Email: {{$user->email}}</p>
+                    @endforeach
                 </div>
-                <hr>
             </div>
         </div>
         <div class="col-md-8 col-sm-12 pull-left posttimeline">
