b32fe6c04df551270626a3af23911e7a39e06bbf
RAG-00 <harry.ragoonaden@gmail.com>
2019-06-11 22:39:47 +1000

    updated about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -23,28 +23,40 @@
   <img class="img-fluid rounded mb-4" src="https://i.imgur.com/V4f1DWx.jpg" alt="">
 </div>
 <div class="col-lg-6">
-  <h2>Meet the Team</h2>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed voluptate nihil eum consectetur similique? Consectetur, quod, incidunt, harum nisi dolores delectus reprehenderit voluptatem perferendis dicta dolorem non blanditiis ex fugiat.</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe, magni, aperiam vitae illum voluptatum aut sequi impedit non velit ab ea pariatur sint quidem corporis eveniet. Odit, temporibus reprehenderit dolorum!</p>
-  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et, consequuntur, modi mollitia corporis ipsa voluptate corrupti eum ratione ex ea praesentium quibusdam? Aut, in eum facere corrupti necessitatibus perspiciatis quis?</p>
+  <h2>What is ArcadiaHub?</h2>
+  <p>ArcadiaHub is a global 100% virtual startup hub connecting startups with investors,
+    teams, influencers, corporates and co-working spaces around the world.
+  </p>
+  <p>ArcadiaHub is more than just a networking platform for startups.</p>
+  <p>
+    <div class="text-center">
+    <strong>CONNECT</strong> 
+    <br>with the <br><strong>RIGHT PEOPLE</strong> 
+    <br>at the <br><strong>RIGHT TIME</strong> 
+    <br>in the <br><strong>RIGHT PLACE</strong><br><br>
+    </div>
+    Join ArcadiaHub’s global virtual startup community today and be part of the global startup revolution.
+  </p>
 </div>
 </div>
 <!-- /.row -->
 
 <!-- Team Members -->
-<h2>Our Team</h2>
+<h2>Features</h2>
 
 <div class="row">
 <div class="col-lg-4 mb-4">
   <div class="card h-100 text-center">
     <img class="card-img-top" src="https://i.imgur.com/zA6OFWY.jpg" alt="">
     <div class="card-body">
-      <h4 class="card-title">Cynthia Collins</h4>
-      <h6 class="card-subtitle mb-2 text-muted">Marketing Director</h6>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
+      <h4 class="card-title">View Startups Globally</h4>
+      <!--<h6 class="card-subtitle mb-2 text-muted">Marketing Director</h6>-->
+      <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>-->
+      <p class="card-text">Learn about the latest innovations from the comfort of your own home</p>
     </div>
     <div class="card-footer">
-      <a href="#">cynthia.collins@arcadiahub.com</a>
+      <!--<a href="#">cynthia.collins@arcadiahub.com</a>-->
+      <a href="#">Get Started</a>
     </div>
   </div>
 </div>
@@ -52,12 +64,14 @@
   <div class="card h-100 text-center">
     <img class="card-img-top" src="https://i.imgur.com/7pipsrA.jpg" alt="">
     <div class="card-body">
-      <h4 class="card-title">Hilary Byrd</h4>
-      <h6 class="card-subtitle mb-2 text-muted">Project Manager</h6>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
+      <h4 class="card-title">Promote Your Business</h4>
+      <!--<h6 class="card-subtitle mb-2 text-muted">Project Manager</h6>-->
+      <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>-->
+      <p class="card-text">Have a marketing reach like never before</p>
     </div>
     <div class="card-footer">
-      <a href="#">hilary.byrd@arcadiahub.com</a>
+      <!--<a href="#">hilary.byrd@arcadiahub.com</a>-->
+      <a href="#">Get Started</a>
     </div>
   </div>
 </div>
@@ -65,12 +79,14 @@
   <div class="card h-100 text-center">
     <img class="card-img-top" src="https://i.imgur.com/0yq2NnK.jpg" alt="">
     <div class="card-body">
-      <h4 class="card-title">Christopher Seefeldt</h4>
-      <h6 class="card-subtitle mb-2 text-muted">Lead Software Engineer</h6>
-      <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>
+      <h4 class="card-title">Build Your Network</h4>
+      <!--<h6 class="card-subtitle mb-2 text-muted">Lead Software Engineer</h6>-->
+      <!--<p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus aut mollitia eum ipsum fugiat odio officiis odit.</p>-->
+      <p class="card-text">Develop the partnerships you always dreamed of</p>
     </div>
     <div class="card-footer">
-      <a href="#">christopher.seef@arcadiahub.com</a>
+      <!--<a href="#">christopher.seef@arcadiahub.com</a>-->
+      <a href="#">Get Started</a>
     </div>
   </div>
 </div>
